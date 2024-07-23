ade79bff4380fb2ed1d19e384a77d0b995914141
rootKid <137388933+devMainak@users.noreply.github.com>
2024-07-23 07:04:01 +0000

    feat: incomes fetched

--- src/pages/Expense.jsx
+++ src/pages/Expense.jsx
@@ -1,6 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchExpenses } from '../actions'
+import { useEffect } from 'react'
+
 const Expense = () => {
+  const dispatch = useDispatch()
+  const expenses = useSelector((state) => state.expenses)
+
+  useEffect(() => {
+    dispatch(fetchExpenses())
+  }, [])
+
+  const totalExpense = expenses.reduce((acc, curr) => {
+    acc += curr.amount
+    return acc
+  }, 0)
+
   return (
-    <div></div>
+    <div>
+      <h1>Expense Page</h1>
+      <ul>
+        {expenses.map((expense, index) => (
+      <li key={index}>
+        {expense.description}: ${expense.amount}
+      </li>
+        ))}
+      </ul>
+      <h2>Expenses Total: ${totalExpense}</h2>
+
+    </div>
   )
 }
 
--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -1,6 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchIncome } from '../actions'
+import { useEffect } from 'react'
+
 const Income = () => {
+  const dispatch = useDispatch()
+  const income = useSelector((state) => state.income)
+
+  useEffect(() => {
+    dispatch(fetchIncome())
+  }, [])
+
+  const totalIncome = income.reduce((acc, curr) => {
+    acc += curr.amount
+    return acc
+  }, 0)
+  
   return (
     <div>
+      <h1>Income Page</h1>
+      <ul>
+        {income.map((salary, index) => (
+      <li key={index}>
+        {salary.description}: ${salary.amount}
+      </li>
+        ))}
+      </ul>
+      <h2>Income Total: ${totalIncome}</h2>
+      
     </div>
   )
 }
